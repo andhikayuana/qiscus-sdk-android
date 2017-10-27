ffae414880f9e469d7acfa32670d605a485d51c6
Andhika Yuana <andhikayuana@gmail.com>
2017-10-27 16:28:42 +0700

    animate switch icon sdk

--- uikit/src/main/java/com/qiscus/sdk/chat/presentation/uikit/widget/QiscusCommentComposer.kt
+++ uikit/src/main/java/com/qiscus/sdk/chat/presentation/uikit/widget/QiscusCommentComposer.kt
@@ -3,11 +3,16 @@ package com.qiscus.sdk.chat.presentation.uikit.widget
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Build
+import android.support.annotation.DrawableRes
 import android.support.annotation.RequiresApi
 import android.support.v4.content.ContextCompat
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import com.qiscus.sdk.chat.presentation.uikit.R
 import kotlinx.android.synthetic.main.view_qiscus_comment_composer.view.*
@@ -22,6 +27,9 @@ class QiscusCommentComposer : FrameLayout {
     private var mView: View? = null
     private var iconEmoticon: Drawable? = null
     private var iconSend: Drawable? = null
+    private var iconAttachment: Drawable? = null
+    private var animBtnSendSwitch: Animation? = null
+    private var isFieldMessageEmpty: Boolean = true
 
     @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr) {
         init(attrs)
@@ -33,7 +41,9 @@ class QiscusCommentComposer : FrameLayout {
     }
 
     private fun init(attrs: AttributeSet?) {
-        inflateView()
+        mView = inflateView()
+
+        animBtnSendSwitch = AnimationUtils.loadAnimation(context, R.anim.qiscus_simple_grow)
 
         val a = this.context.theme.obtainStyledAttributes(
                 attrs,
@@ -46,6 +56,7 @@ class QiscusCommentComposer : FrameLayout {
 //            TODO : styleable
             iconEmoticon = a.getDrawable(R.styleable.QiscusCommentComposer_btnEmoticonIcon)
             iconSend = a.getDrawable(R.styleable.QiscusCommentComposer_btnSendIcon)
+            iconAttachment = a.getDrawable(R.styleable.QiscusCommentComposer_btnAttachment)
 
             initDefaultAttrs()
 
@@ -55,9 +66,12 @@ class QiscusCommentComposer : FrameLayout {
     }
 
     private fun initDefaultAttrs() {
-        if (iconEmoticon == null) iconEmoticon = ContextCompat.getDrawable(context, R.drawable.ic_qiscus_emot)
-        if (iconSend == null) iconSend = ContextCompat.getDrawable(context, R.drawable.ic_qiscus_attach)
+        if (iconEmoticon == null) iconEmoticon = getDrawable(R.drawable.ic_qiscus_emot)
+        if (iconSend == null) iconSend = getDrawable(R.drawable.ic_qiscus_send)
+        if (iconAttachment == null) iconAttachment = getDrawable(R.drawable.ic_qiscus_attach)
     }
+
+    private fun getDrawable(@DrawableRes icon: Int) = ContextCompat.getDrawable(context, icon)
 
     override fun onFinishInflate() {
         super.onFinishInflate()
@@ -74,18 +88,44 @@ class QiscusCommentComposer : FrameLayout {
         commentComposerInsertEmoticon.setOnClickListener {
             callback.onClickInsertEmoticon(it)
         }
+        commentComposerTextField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                onMessageEditTextChanged(s)
+            }
+        })
+    }
+
+    private fun onMessageEditTextChanged(message: CharSequence?) {
+        if (message == null || message.toString().trim().isEmpty()) {
+            if (!isFieldMessageEmpty) {
+                isFieldMessageEmpty = true
+                commentComposerButtonSend.startAnimation(animBtnSendSwitch)
+                commentComposerButtonSend.setImageDrawable(iconAttachment)
+            }
+        } else {
+            if (isFieldMessageEmpty) {
+                isFieldMessageEmpty = false
+                commentComposerButtonSend.startAnimation(animBtnSendSwitch)
+                commentComposerButtonSend.setImageDrawable(iconSend)
+            }
+        }
     }
 
     private fun displayAttrs() {
 //        todo
         commentComposerInsertEmoticon.setImageDrawable(iconEmoticon)
-        commentComposerButtonSend.setImageDrawable(iconSend)
+        commentComposerButtonSend.setImageDrawable(iconAttachment)
         invalidateAndRequestLayout()
     }
 
-    private fun inflateView() {
-        mView = getInflater().inflate(R.layout.view_qiscus_comment_composer, this)
+    private fun inflateView(): View {
+        return getInflater().inflate(R.layout.view_qiscus_comment_composer, this)
     }
 
     private fun getInflater(): LayoutInflater {
